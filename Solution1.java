b130e752110d3618e3d4dd32551716a574b8a04c
Gracysinngh <gracysinngh27.com>
2026-01-10 10:57:50 +0530

    Question 2

---
+++ Solution1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+      Scanner sc= new Scanner(System.in);
+      int num1=sc.nextInt();
+      int num2=sc.nextInt();
+
+      num1=num1+num2;
+      num2=num1-num2;
+      num1=num1-num2;
+
+      System.out.println("After swapping: " + num1 + " "+ num2);
+    }
+}
